c5fcad8c0be4876e0e089991306a28d50f730c6e
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 18:51:16 +0200

    úprava velikosti textu

--- src/displayKey/DisplayKey.js
+++ src/displayKey/DisplayKey.js
@@ -18,10 +18,10 @@ const DisplayKey = () => {
 
     return (
         <div className="flex flex-col justify-center items-center pt-12">
-            <h2 className="text-foreColorWhite text-3xl flex justify-center items-center pt-4 uppercase">
+            <h2 className="text-foreColorWhite text-xl flex justify-center items-center pt-4 uppercase">
                  Zmáčkl jsi klávesu: " {pressedKey} "
             </h2>
-            <div className="border-b-2 border-foreColorWhite w-[40%] pt-2">
+            <div className="border-b-2 border-foreColorWhite w-[30%] pt-2">
 
             </div>
         </div>
