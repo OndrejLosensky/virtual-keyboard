faf4f53e0b41e9f3f123335337c3df8fb4ac447f
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 13:54:21 +0200

    přidání tlačítka

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,8 +3,9 @@ import React from "react";
 const Header = () => {
     return (
         <div className="w-full h-[80px] border-b-[1px] border-b-foreColorWhite">
-            <div className="text-foreColorWhite">
+            <div className="text-foreColorWhite flex flex-row justify-center">
                 <h1 className="uppercase text-4xl text-center py-4 font-semiBold"> Virtuální klávesnice</h1>
+                <button className=""> Obnovit </button>
             </div>
         </div>
     )
