b53d8435ce646efad482c4ecc0476d23fd3cc1f3
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 19:00:42 +0200

    úprava velikosti padding-y

--- src/header/Header.js
+++ src/header/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
             */}
 
             <div className="w-[100%] h-[60px] border-b-[1px] border-b-foreColorWhite  flex items-center">
-                <h1 className="w-full uppercase text-4xl text-center py-6 font-bold text-foreColorWhite">
+                <h1 className="w-full uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
                     Virtuální klávesnice
                 </h1>
             </div>
