cb5f8e671d1990ac0e823e73ad48b1948f8889f6
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 13:51:00 +0200

    vytvoření komponenty header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Header from "../src/header/Header"
 
 function App() {
   return (
     <section className="w-full h-screen bg-backColor">
-      <div className="text-foreColorWhite">
-        <h1 className="uppercase text-4xl text-center py-4 font-semiBold"> Virtuální klávesnice</h1>
-      </div>
+          <Header/>
     </section>
   );
 }
--- src/header/Header.js
+++ src/header/Header.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="w-full h-[80px] border-b-[1px] border-b-foreColorWhite">
+            <div className="text-foreColorWhite">
+                <h1 className="uppercase text-4xl text-center py-4 font-semiBold"> Virtuální klávesnice</h1>
+            </div>
         </div>
     )
 }
