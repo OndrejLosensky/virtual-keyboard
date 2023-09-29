26d5a4544bd1b5964f11731a6c27d1ca486ce84f
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 15:03:11 +0200

    první verze rozpoložení virtuální klávesnice

--- src/keyboard/Keyboard.js
+++ src/keyboard/Keyboard.js
@@ -3,7 +3,22 @@ import ButtonKey from "../button/ButtonKey";
 
 const Keyboard = () => {
     return (
-        <div className=" bg-foreColorBlue flex flex-col gap-2" >
+        <div className=" h-[88%] flex flex-col gap-2 justify-center items-center" >
+
+            <div className="flex flex-row gap-2">
+                <ButtonKey text="1"/>
+                <ButtonKey text="2"/>
+                <ButtonKey text="3"/>
+                <ButtonKey text="4"/>
+                <ButtonKey text="5"/>
+                <ButtonKey text="6"/>
+                <ButtonKey text="7"/>
+                <ButtonKey text="8"/>
+                <ButtonKey text="9"/>
+                <ButtonKey text="10"/>
+                <ButtonKey text="delete"/>
+            </div>
+        
             <div className="flex flex-row gap-2">
                 <ButtonKey text="Q"/>
                 <ButtonKey text="W"/>
@@ -15,9 +30,11 @@ const Keyboard = () => {
                 <ButtonKey text="i"/>
                 <ButtonKey text="o"/>
                 <ButtonKey text="p"/>
+                <ButtonKey text="enter"/>
             </div>
 
             <div className="flex flex-row gap-2">
+                <ButtonKey text="caps"/>
                 <ButtonKey text="a"/>
                 <ButtonKey text="s"/>
                 <ButtonKey text="d"/>
@@ -27,6 +44,7 @@ const Keyboard = () => {
                 <ButtonKey text="j"/>
                 <ButtonKey text="k"/>
                 <ButtonKey text="l"/>
+                <ButtonKey text="ů"/>
             </div>
                 
             <div className="flex flex-row gap-2">
@@ -37,6 +55,7 @@ const Keyboard = () => {
                 <ButtonKey text="b"/>
                 <ButtonKey text="n"/>
                 <ButtonKey text="m"/>
+                <ButtonKey text="shift"/>
             </div>        
         </div>
        
