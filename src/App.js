ead52ced23baee8041f2dae5455efee8d8e8ffbe
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 14:47:45 +0200

    přidání tlačítek klávesnice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "../src/header/Header"
+import Keyboard from "./keyboard/Keyboard";
 
 function App() {
   return (
     <section className="w-full h-screen bg-backColor">
           <Header/>
+          <Keyboard/>
     </section>
   );
 }
---
+++ src/keyboard/Keyboard.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Keyboard = () => {
+    return (
+        <div className=" bg-foreColorBlue justify-center items-center">
+            <div class="grid grid-cols-9 gap-x-2 gap-y-2 justify-items-center pt-4">
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">q</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">w</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">e</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">r</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">t</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">z</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">u</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">i</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">o</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">p</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">a</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">s</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">d</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">f</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">g</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">h</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">j</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">k</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">l</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">y</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">x</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">c</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">v</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">b</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">n</button>
+                <button class="px-4 py-4 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500">m</button>
+            </div>
+        </div>
+       
+    
+    )
+}
+
+export default Keyboard
