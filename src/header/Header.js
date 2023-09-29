e124a881327c5a59f6519ce1650c66a434519abf
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 14:01:37 +0200

    nadpis v centru, tlacitko nalevo

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import RefreshIcon from "../assets/refreshIcon.png"
 const Header = () => {
     return (
-        <div className="w-full h-[80px] border-b-[1px] border-b-foreColorWhite">
-            <div className="text-foreColorWhite flex flex-row justify-center">
-                <h1 className="uppercase text-4xl text-center py-4 font-semiBold"> Virtuální klávesnice</h1>
-                <button className=""> Obnovit </button>
-            </div>
+        <div className="w-full h-[80px] border-b-[1px] border-b-foreColorWhite flex justify-between items-center">
+            
+                <h1 className="uppercase text-4xl text-center py-4 font-semiBold mx-auto text-foreColorWhite">
+                     Virtuální klávesnice
+                </h1>
+
+                <button className="px-8"> 
+                    <img src={RefreshIcon} alt="refresh" height={30} width={30}/>
+                </button>
+           
         </div>
     )
 }
