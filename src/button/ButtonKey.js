95d4b5c764c50e6bb525b8a816d66e2e73677e4a
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-29 15:53:43 +0200

    úprava vzhledu při hover

--- src/button/ButtonKey.js
+++ src/button/ButtonKey.js
@@ -3,7 +3,11 @@ import React from "react";
 const ButtonKey = (params) => {
     return (
         <div className="text-foreColorWhite">
-             <button class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-lg border-2 border-gray-500 uppercase hover:bg-foreColorWhite hover:text-backColor">{params.text}</button>
+            <button class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-lg 
+             border-2 border-gray-500 uppercase hover:bg-foreColorWhite hover:text-backColor
+             hover:border-2 border-foreColorWhite duration-300">
+                {params.text}
+            </button>
         </div>
     )
 }
