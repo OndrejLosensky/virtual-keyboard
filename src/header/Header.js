b21756c98e8384ee8baad00b8f929044e59f9762
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 14:20:53 +0200

    změna tloušťky fontu

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="px-8 pt-2">
             <div className="w-[100%] h-[60px] border-b-[2px] border-b-foreColorWhite  flex items-center">
-                <h1 className="w-full md:font-thin xl:font-bold lg:font-semiBold sm:font-extralight uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
+                <h1 className="w-full tablet:font-thin laptop:font-semiBold bigScreen:font-bold uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
                     Virtuální klávesnice
                 </h1>
             </div>
