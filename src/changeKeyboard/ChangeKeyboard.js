c9c509ed12c3c087016b3c99899b1d49ad62067b
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 16:08:12 +0200

    vytvoření dropdown tlačítka pro změnu klávesnice

---
+++ src/changeKeyboard/ChangeKeyboard.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const ChangeKeyboard = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className="relative inline-block text-left">
+      <button
+        onClick={toggleDropdown}
+        className=" text-white py-2 px-4 rounded-md"
+      >
+        Změnit klávesnici
+      </button>
+      {isDropdownOpen && (
+        <div className="absolute right-0 mt-4 w-40 bg-white border shadow-md rounded-md">
+
+          <ul>
+            <li>
+              <button className="block w-full py-2 px-4 text-left">
+                Česká
+              </button>
+            </li>
+            <li>
+              <button className="block w-full py-2 px-4 text-left">
+                Anglická
+              </button>
+            </li>
+
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ChangeKeyboard
