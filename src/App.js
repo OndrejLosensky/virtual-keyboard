e74cf4ac2c0a388650b9baf79f98c51465abb46b
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 11:59:06 +0200

    import nové verze obrázku

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "../src/header/Header"
 import Keyboard from "./keyboard/Keyboard";
-
+import backgroundImage from "./assets/backgroundImage.png"
 function App() {
   return (
-    <section className="w-full h-screen lg:h-full bg-backColor">
+    <section className="w-full h-screen lg:h-full" style={{ 
+      backgroundImage: `url(${backgroundImage})`,
+    }}>
           <Header/>
           <Keyboard/>
     </section>
