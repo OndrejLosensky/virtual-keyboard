68c5aaa8e80202427ea022c93d8573156c062354
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 15:28:52 +0200

    automatický FOCUS na Input

--- src/inputKeyboard/InputKeyboard.js
+++ src/inputKeyboard/InputKeyboard.js
@@ -5,16 +5,26 @@ const InputKeyboard = () => {
     const inputRef = useRef(null);
 
     useEffect(() => {
-        inputRef.current.focus();
+        // Reaguje na vstup z klávesnice
+        const focusInput = () => {
+            inputRef.current.focus();
+        };
+
+        // Reaguje při kliknutí a automaticky zaměří Input pro automatické psaní
+        document.addEventListener("click", focusInput);
+
+        return () => {
+            document.removeEventListener("click", focusInput);
+        };
     }, []);
 
     return (
-        <div className="px-40 flex flex-col justify-center items-center text-center pt-12">
+        <div className="flex flex-col justify-center items-center text-center pt-12">
             <input
                 ref={inputRef}
                 type="text"
-                className="text-foreColorWhite outline-none text-2xl bg-backColor"
-                placeholder="Zde se zobrazí vámi napsaný text"
+                className="text-foreColorWhite outline-none text-2xl bg-backColor py-2"
+                placeholder="Zde se zobrazí psaný text"
             />
             <div className="border-b-2 border-foreColorWhite w-[40%]"></div>
         </div>
