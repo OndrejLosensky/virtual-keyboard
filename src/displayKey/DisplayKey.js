eeb619b71e1a69bb9e16a36158365d528782013b
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 15:35:23 +0200

    změna zobrazování stisknuté klávesy

--- src/displayKey/DisplayKey.js
+++ src/displayKey/DisplayKey.js
@@ -17,10 +17,11 @@ const DisplayKey = () => {
     }, []);
 
     return (
-        <div className="flex flex-col justify-center items-center pt-12">
-            <h2 className="text-foreColorWhite text-lg flex justify-center items-center pt-4 uppercase">
-                 Zmáčkl jsi klávesu: " {pressedKey} "
+        <div className="flex flex-col justify-center items-center pt-8">
+            <h2 className="text-foreColorWhite text-xl flex justify-center items-center pt-4 uppercase flex-row">
+                 Zmáčkl jsi klávesu:
             </h2>
+            <p className="text-lg text-foreColorWhite"> {pressedKey} </p>
             <div className="border-b-2 border-foreColorWhite w-[100%] pt-2">
 
             </div>
