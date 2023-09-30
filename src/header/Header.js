debc825783c71f83d402d4b4f98d2d6c0f7ce1d8
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 12:12:15 +0200

    zvětšení border

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className="px-8 pt-2">
-            <div className="w-[100%] h-[60px] border-b-[1px] border-b-foreColorWhite  flex items-center">
+            <div className="w-[100%] h-[60px] border-b-[2px] border-b-foreColorWhite  flex items-center">
                 <h1 className="w-full md:font-thin xl:font-bold lg:font-semiBold sm:font-extralight uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
                     Virtuální klávesnice
                 </h1>
