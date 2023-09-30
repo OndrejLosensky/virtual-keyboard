4e31cbe1fac8827ab07073b5bda9504545a8ae4d
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 18:01:27 +0200

    změna nadpisu z font-thin na font-normal

--- src/header/Header.js
+++ src/header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <div className="px-8 pt-2">
             <div className="w-[100%] h-[60px] border-b-[2px] border-b-foreColorWhite  flex items-center">
-                <h1 className="w-full tablet:font-thin laptop:font-semiBold bigScreen:font-bold uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
+                <h1 className="w-full tablet:font-normal laptop:font-semiBold bigScreen:font-bold uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
                     Virtuální klávesnice
                 </h1>
             </div>
