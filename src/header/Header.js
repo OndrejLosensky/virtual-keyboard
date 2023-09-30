0b74627b065a9612e9bd664489760aff6500e684
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 12:11:59 +0200

    nastavení padding pro nadpis

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 //import RefreshIcon from "../assets/refreshIcon.png"
 const Header = () => {
     return (
-        <div className="">
+        <div className="px-8 pt-2">
             <div className="w-[100%] h-[60px] border-b-[1px] border-b-foreColorWhite  flex items-center">
                 <h1 className="w-full md:font-thin xl:font-bold lg:font-semiBold sm:font-extralight uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
                     Virtuální klávesnice
