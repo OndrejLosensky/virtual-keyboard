061d9c36b10dfc3505419f94257f049454ef5782
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-30 17:55:29 +0200

    vytvoření prokliku na spuštění testu

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
 //import RefreshIcon from "../assets/refreshIcon.png"
 const Header = () => {
+
+   // funkce bude zavolána po kliknutí na "zde"
+   const handleClick = () => {
+
+    alert("Tato funkce momentálně není ještě dostupná");
+  };
+
     return (
         <div className="px-8 pt-2">
             <div className="w-[100%] h-[60px] border-b-[2px] border-b-foreColorWhite  flex items-center">
@@ -9,11 +16,12 @@ const Header = () => {
                 </h1>
             </div>
             <div>
-                <p className="text-foreColorWhite pt-6 flex justify-center text-center text-lg font-thin">
+                <p className="text-foreColorWhite pt-6  text-center text-lg font-thin">
 
                     Jednoduchá virtuální klávesnice, která zobrazuje jaké klávesy stisknete,<br/>
                     a také obsahuje funkci pro otestování funkčnosti vaší klávesnice <br/>
-                    klávesnice je dostupná v českém rozložení a anglickém a ve verzích na Mac a Windows
+                    klávesnice je dostupná v českém rozložení a anglickém a ve verzích na Mac a Windows <br/>
+                    kliknutím <span className="cursor-pointer text-foreColorOrange font-semibold" onClick={handleClick} style={{ textDecoration: "underline" }}> zde</span> spustíte test funkčnosti vaší klávesnice
                 </p>
             </div>
        </div>
