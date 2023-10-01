31bb1476677503424bd274f0bc4863d9b604b812
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-01 18:38:53 +0200

    úprava funkčnosti tlačítka

--- src/components/changeKeyboard/ChangeKeyboard.js
+++ src/components/changeKeyboard/ChangeKeyboard.js
@@ -1,37 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const ChangeKeyboard = ({ onLayoutChange }) => {
-  
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  // Tato funkce automaticky zavře dropdown menu jakmile kliknu kamkoliv vedle
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+    setIsDropdownOpen(!isDropdownOpen); // tato funkce spouští dropdown menu
   };
 
+  const handleLayoutItemClick = (layout) => {
+    onLayoutChange(layout); // změna layout při kliknutí na tlačítko
+    setIsDropdownOpen(false); // zavře dropdown menu jakmile kliknu na tlačítko
+  };
 
   return (
-    <div className="relative inline-block text-left">
+    <div className="relative inline-block text-left" ref={dropdownRef}>
       <button
         onClick={toggleDropdown}
-        className=" text-white py-2 px-4 rounded-md"
+        className="text-white py-2 px-4 rounded-md"
       >
         Změnit klávesnici
       </button>
       {isDropdownOpen && (
         <div className="absolute right-0 mt-4 w-40 bg-white border shadow-md rounded-md">
-
           <ul>
             <li>
-              <button className="block w-full py-2 px-4 text-left hover:bg-btnbg duration-300" onClick={() => onLayoutChange("czech")}>
+              <button
+                className="block w-full py-2 px-4 text-left hover:bg-btnbg duration-300"
+                onClick={() => handleLayoutItemClick("czech")}
+              >
                 Česká [CZ]
               </button>
             </li>
             <li>
-              <button className="block w-full py-2 px-4 text-left hover:bg-btnbg duration-300" onClick={() => onLayoutChange("english")}>
+              <button
+                className="block w-full py-2 px-4 text-left hover:bg-btnbg duration-300"
+                onClick={() => handleLayoutItemClick("english")}
+              >
                 Anglická [ENG]
               </button>
             </li>
-
           </ul>
         </div>
       )}
@@ -39,4 +62,4 @@ const ChangeKeyboard = ({ onLayoutChange }) => {
   );
 };
 
-export default ChangeKeyboard
+export default ChangeKeyboard;
