1578f1ea0701afd89010f79bcb14116419b28ffa
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-07 14:43:11 +0200

    změna nadpisu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
     return (
         <div className="px-8 pt-2">
             <div className="w-[100%] h-[60px] border-b-[2px] border-b-foreColorWhite  flex items-center">
-                <h1 className="w-full tablet:font-normal laptop:font-semiBold bigScreen:font-bold uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
-                    Virtuální klávesnice
+                <h1 className="w-full tablet:font-normal laptop:font-bold bigScreen:font-bold uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
+                    Virtual-keyboard
                 </h1>
             </div>
             <div>
