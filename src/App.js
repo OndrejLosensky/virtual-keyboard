5b23f0a5e42507d3a3a1f29914a478a8122053af
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 19:04:55 +0200

    rozšíření dokumentace

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header";
 import Keyboard from "./components/keyboard/Keyboard";
-import backgroundImage from "./assets/backgroundImage.png"
+//import backgroundImage from "./assets/backgroundImage.png"
 import { useState } from "react";
 
 function App() {
@@ -32,6 +32,16 @@ function App() {
               Najdete zde návod, obsah funkcí i chyby, které aplikace má <br/>
               také bude ke stažení samostatná klávesnice jako NPM PACKAGE
             </p>
+            <div className="text-center">
+              <h3 className="uppercase font-bold text-2xl py-2"> Funkce</h3>
+                <p> <span className="font-semibold text-xl">Klávesnice má zkratky pro ovládání:</span> <br/> CTRL + SHIFT  =  Změní typ klávesnice <br/> CTRL + R  =  Obnoví stránku</p>
+              <h3 className="uppercase font-bold text-2xl text-center py-2"> Vzhled</h3>
+                <p> <span className="font-semibold text-xl">Seznam všech použitých barev: </span> <br/> Barva pozadí : "#ffffff" <br/> Barva textu : "#ffffff" <br/> Barva kláves : "#ffffff" <br/> </p>
+              <h3 className="uppercase font-bold text-2xl text-center py-2"> Ovládání</h3>
+                <p><span className="font-semibold text-xl"> Popis ovládání virtuální klávesnice:</span> <br/> klávesnice reaguje na stisky reálných kláves <br/> při kliknutí se rozsvítí klávesa červeně a vypíše dané písmenko do inputu <br/> Lze spustit test, který vyhodnotí zda máte funkční klávesnici </p>
+              <h3 className="uppercase font-bold text-2xl text-center py-2"> Chyby</h3>
+                <p> <span className="font-semibold text-xl"> Chyby, které se v aplikaci vyskytují: </span> <br/> - aplikace občas nezaznamenává stisky virtuálních kláves <br/> - klávesy jako je COMMAND nebo OPTION nefungují ani se nezobrazují <br/> - dokumentace nemá scrollbar z důvodu vypnutého scrollování klávesnice </p>
+            </div>
           </div>
          
         </div>
