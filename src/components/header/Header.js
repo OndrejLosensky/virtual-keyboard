2cd75fa7ee33bb8f13ce9c2b2e8117489e41cc46
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 19:09:50 +0200

    upravení odsazení

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 
 
     return (
-        <div className="px-8 pt-2">
+        <div className="pl-20 pr-4 pt-2">
             <div className="w-[100%] h-[60px] border-b-[2px] border-b-foreColorWhite  flex items-center">
                 <h1 className="w-full tablet:font-normal laptop:font-bold bigScreen:font-bold uppercase text-4xl text-center py-10 font-bold text-foreColorWhite">
                     Virtual-keyboard
