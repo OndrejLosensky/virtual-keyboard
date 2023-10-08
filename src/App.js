1a96a8c9e50a8af58fa30d3fb39cffa2cac1b95a
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 17:43:30 +0200

    přidání sekce s dokumentací

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ function App() {
     <section className="w-full h-screen lg:h-full bg-backColor" style={{ 
       backgroundImage: `url(${backgroundImage})`,
     }}>
-          <Header/>
-          <Keyboard/>
+      <div className="flex flex-row">
+        <div className="w-[60%]">
+            <Header/>
+            <Keyboard/>
+        </div>
+        <div className="w-[40%] bg-foreColorWhite">
+           <h2 className="text-4xl uppercase text-center pt-6 font-semibold"> dokumentace</h2>
+        </div>
+      </div>
     </section>
     
   );
